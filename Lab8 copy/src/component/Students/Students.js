327234701834fa84aefb86c4435dba1b092a1057
Sirak <sirak123hw@gmail.com>
2021-09-14 19:06:29 -0500

    lab 8

---
+++ Lab8 copy/src/component/Students/Students.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Students.css';
+
+const Students = (props) =>{
+
+    return (
+        <div className = "Students">
+            <h3> Student</h3>
+            <p>id : {props.id}</p>
+            <p>Name : {props.name}</p>
+            <p>Major : {props.major}</p>
+        </div>
+    );
+}
+
+export default Students;
